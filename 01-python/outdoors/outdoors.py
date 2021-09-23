23e579ebac3aeac9cc8a4c160daba1d3bb85e413
syu25 <syu25@ncsu.edu>
2021-09-23 09:54:36 -0400

    add outdoor components

--- 01-python/outdoors/outdoors.py
+++ 01-python/outdoors/outdoors.py
@@ -1,7 +1,9 @@
 from .lake import draw_lake
 from .park import draw_park
+from .mountain import draw_mountain
 
 def draw_outdoors():
     draw_lake()
     draw_park()
+    draw_mountain()
     return
